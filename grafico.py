eab77a88e5192d03d0b43751fc7fa61865681e71
leonardo-vinicius <lvwjs@ic.ufal.br>
2023-04-18 09:47:50 -0300

    graficos_edfa1_e_edfa2

--- grafico.py
+++ grafico.py
@@ -3,14 +3,17 @@ import numpy as np
 import os
 
 
-def gerar_graficos(file_AM, file_PM, flag, n_span, n_km, n_canais):
+def gerar_graficos(file_AM, file_PM, file_AM2, file_PM2, flag, n_span, n_km, n_canais):
 
     plt.rcParams.update({'figure.max_open_warning': 0})
-
-    espacamento =  (max(file_AM) - min(file_PM)) * 0.20
-    menor_valor = min(min(file_AM), min(file_PM)) - espacamento
-    maior_valor = max(max(file_AM), max(file_PM)) + espacamento
-
+    maximo = [ max(file_AM), max(file_AM2), max(file_PM), max(file_PM2)]
+    minimo = [ min(file_AM), min(file_AM2), min(file_PM), min(file_PM2)]
+    """ print(maximo)
+    print(minimo) """
+    espacamento =  (max(maximo)) - (min(minimo)) * 0.20
+    menor_valor = min(minimo) - espacamento
+    maior_valor = max(maximo) + espacamento
+    print(f'Espacamento {menor_valor} - {maior_valor}')
     print(n_span)
 
     if n_km == '1':
@@ -26,24 +29,29 @@ def gerar_graficos(file_AM, file_PM, flag, n_span, n_km, n_canais):
     plot1 = fig.add_subplot()
     plot1 = plt.subplot()
     plot1.grid(True, linestyle=':', linewidth=0.5)
-    plot1.scatter(x, file_PM, color= 'blue', label = 'Power Mask Model', linewidth=0.5, edgecolors='azure')
+    plot1.scatter(x, file_PM, color= 'blue', label = 'Power Mask Model edfa1', linewidth=0.5, edgecolors='azure')
     plt.plot(x,file_PM,color='purple', alpha=0.5) # antes tava azure mas o fundo é claro agora
-    plot1.scatter(x, file_AM, color= 'gold', label = 'Advanced Model GNPy', linewidth=0.5, edgecolors='orange')
+    plot1.scatter(x, file_AM, color= 'gold', label = 'Advanced Model GNPy edfa1', linewidth=0.5, edgecolors='orange')
     plot1.tick_params(labelsize=14) ##
+
+    # mudanças gerar 4 graficos
+    plot1.scatter(x, file_AM2, color= 'red', label = 'Advanced Model GNPy edfa2', linewidth=0.5)
+    plot1.scatter(x, file_PM2, color= 'green', label = 'Power Mask Model edfa2', linewidth=0.5)
+    ##
+
     plt.plot(x,file_AM,color='red', alpha=0.5)
     plt.ylim(menor_valor, maior_valor)
     plot1.set_ylabel(flag + ' (dB)\n', fontsize=16)
     plot1.set_title(flag + ' (dB)' + n_canais + '0 ch - ' + n_span + ' Span ' + n_km + '0Km\n', fontsize = 16)
     plot1.set_xlabel('\nFrequency Channel (THz)', fontsize=16)
     plot1.legend(prop = { "size": 16 })
-    #plot1.set_facecolor(lightGray) #para deixar com fundo cinza
     #plt.subplots_adjust(left=0.1, bottom=0.1,  right=0.9,  top=0.9, wspace=0.4,  hspace=0.4) 
 
-    ##### Para deixar versão png desmarcar linha 46 e marcar linha 45 e desmarcar linha 51 e marcar linha 50
 
+    ##### Para deixar versão png desmarcar linha 46 e marcar linha 45 e desmarcar linha 51 e marcar linha 50
     current_path = os.path.dirname(os.path.realpath(__file__))
     #directory_path = current_path + "/graficos_gerados_pdf/edfa2/"
-    directory_path = current_path + "/graficos_gerados_pdf/edfa1/"
+    directory_path = current_path + "/graficos_gerados_pdf/edfa_ambos/"
 
     print(directory_path)
     nome = 'Grafico_' + flag + '_' + n_canais + '0ch_' + n_span + '_Span_' + n_km + '0Km'
--- plotagem.py
+++ plotagem.py
@@ -11,9 +11,11 @@ arquivo = []
 
 #directory_path = current_path + "/graficos_gerados_csv/edfa1"
 directory_path = current_path + "/graficos_gerados_csv/edfa1/"
+directory_path2 = current_path + "/graficos_gerados_csv/edfa2"
 files = os.listdir(directory_path)
+files2 = os.listdir(directory_path2)
 
-def abrir_arquivo(arquivo_AM, arquivo_PM, n_span, n_km, n_canais):
+def abrir_arquivo(arquivo_AM, arquivo_PM, arq_AM2, arq_PM2, n_span, n_km, n_canais):
 
     CHPout_AM1 = []
     GSNR_AM1 = []
@@ -21,8 +23,17 @@ def abrir_arquivo(arquivo_AM, arquivo_PM, n_span, n_km, n_canais):
     CHPout_PM1 = []
     GSNR_PM1 = []
 
+    CHPout_AM2 = []
+    GSNR_AM2 = []
+
+    CHPout_PM2 = []
+    GSNR_PM2 = []
+
     arquivo_AM = directory_path + arquivo_AM 
     arquivo_PM = directory_path + arquivo_PM
+
+    arquivo_AM2 = directory_path2 + arq_AM2 
+    arquivo_PM2 = directory_path2 + arq_PM2
 
     with open(arquivo_AM, mode='r') as arq:
         leitor = csv.reader(arq, delimiter=';')
@@ -34,6 +45,17 @@ def abrir_arquivo(arquivo_AM, arquivo_PM, n_span, n_km, n_canais):
             linhas += 1
     print(f'acabou. lidas {linhas} lidas.')
 
+    with open(arquivo_AM, mode='r') as arq:
+        leitor = csv.reader(arq, delimiter=';')
+        linhas = 0
+        for coluna in leitor:
+            if linhas != 0:
+                CHPout_AM2.append((float(coluna[2])))
+                GSNR_AM2.append((float(coluna[5])))
+            linhas += 1
+    print(f'acabou. lidas {linhas} lidas.')
+
+
     with open(arquivo_PM, mode='r') as arq:
         leitor = csv.reader(arq, delimiter=';')
         linhas = 0
@@ -44,25 +66,49 @@ def abrir_arquivo(arquivo_AM, arquivo_PM, n_span, n_km, n_canais):
             linhas += 1
     print(f'acabou. lidas {linhas} lidas.\n gerando grafico.')
 
+    with open(arquivo_PM, mode='r') as arq:
+        leitor = csv.reader(arq, delimiter=';')
+        linhas = 0
+        for coluna in leitor:
+            if linhas != 0:
+                CHPout_PM2.append((float(coluna[2])))
+                GSNR_PM2.append((float(coluna[5])))
+            linhas += 1
+    print(f'acabou. lidas {linhas} lidas.\n gerando grafico.')
 
-    grafico.gerar_graficos(CHPout_AM1, CHPout_PM1, 'Channel Power', n_span, n_km, n_canais)
-    grafico.gerar_graficos(GSNR_AM1, GSNR_PM1, 'GSNR', n_span, n_km, n_canais)
+
+    grafico.gerar_graficos(CHPout_AM1, CHPout_PM1, CHPout_AM2, CHPout_PM2, 'Channel Power', n_span, n_km, n_canais)
+    grafico.gerar_graficos(GSNR_AM1, GSNR_PM1, GSNR_AM1, GSNR_AM2, 'GSNR', n_span, n_km, n_canais)
 
 for file in files:
     arquivo.append(file) 
     name = file
 
-def passar_parametros(chave, valor1, valor2):
+######### adicionando todos os arquivos juntos em files
+for file in files2:
+    arquivo.append(file) 
+    name = file
+
+def passar_parametros(chave, valor1, valor2, valor3, valor4):
     arquivo1 = valor1
     arquivo2 = valor2
     n_canais = chave[len(chave) - 2]
     n_span = valor1[0] #                 
     n_km = valor1[8]   #  
     print(f"numero canais {n_canais} numero span {n_span} numero km {n_km}\n")
-    abrir_arquivo(arquivo1, arquivo2, n_span, n_km, n_canais)
+    abrir_arquivo(arquivo1, arquivo2, valor3, valor4, n_span, n_km, n_canais)
+
 
 
 dic = {
+            '1_span_90_40': ['1_spans_90.0 Km_new_model_example_40ch.csv', '1_spans_90.0 Km_EDFA_PADTEC_AdvancedModel_40ch.csv', '1_spans_90.0 Km_edfa2_AdvancedModel40ch.csv', '1_spans_90.0 Km_edfa2_mask_model40ch.csv'],
+            '1_span_110_40': ['1_spans_110.0 Km_new_model_example_40ch.csv', '1_spans_110.0 Km_EDFA_PADTEC_AdvancedModel_40ch.csv', '1_spans_110.0 Km_edfa2_AdvancedModel40ch.csv', '1_spans_110.0 Km_edfa2_mask_model40ch.csv'],
+            '4_span_90_40': ['4_spans_90.0 Km_new_model_example_40ch.csv', '4_spans_90.0 Km_EDFA_PADTEC_AdvancedModel_40ch.csv', '4_spans_90.0 Km_edfa2_AdvancedModel40ch.csv', '4_spans_90.0 Km_edfa2_mask_model40ch.csv'],
+            '4_span_110_40': ['4_spans_110.0 Km_new_model_example_40ch.csv', '4_spans_110.0 Km_EDFA_PADTEC_AdvancedModel_40ch.csv', '4_spans_110.0 Km_edfa2_AdvancedModel40ch.csv', '4_spans_110.0 Km_edfa2_mask_model40ch.csv']
+           
+        } 
+
+""" dic = {
             '1_span_70_40': ['1_spans_70.0 Km_new_model_example_40ch.csv', '1_spans_70.0 Km_EDFA_PADTEC_AdvancedModel_40ch.csv'],
             '1_span_70_80': ['1_spans_70.0 Km_new_model_example_80ch.csv', '1_spans_70.0 Km_EDFA_PADTEC_AdvancedModel_80ch.csv'],
             '1_span_90_40': ['1_spans_90.0 Km_new_model_example_40ch.csv', '1_spans_90.0 Km_EDFA_PADTEC_AdvancedModel_40ch.csv'],
@@ -75,7 +121,7 @@ dic = {
             '4_span_90_80': ['4_spans_90.0 Km_new_model_example_80ch.csv', '4_spans_90.0 Km_EDFA_PADTEC_AdvancedModel_80ch.csv'],
             '4_span_110_40': ['4_spans_110.0 Km_new_model_example_40ch.csv', '4_spans_110.0 Km_EDFA_PADTEC_AdvancedModel_40ch.csv'],
             '4_span_110_80': ['4_spans_110.0 Km_new_model_example_80ch.csv', '4_spans_110.0 Km_EDFA_PADTEC_AdvancedModel_80ch.csv']
-        } 
+        }  """
 
 """ dic = {
             
@@ -92,4 +138,4 @@ dic = {
 for chave, valor in dic.items():
     print(f"Chave = {chave} - Valor 1 = {valor[1]} - Valor 2 = {valor[0]}")
     print(f"Gerando grafico...{chave}\n")
-    passar_parametros(chave, valor[1], valor[0])
+    passar_parametros(chave, valor[1], valor[0], valor[2], valor[3])
